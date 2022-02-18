661f4246f13925fee495f19995f8ae6667d5c403
Sombaran Gupta <40040729+Sombaran@users.noreply.github.com>
2022-02-18 16:18:09 +0530

    Create pushing_vector_into_set.cpp

---
+++ pushing_vector_into_set.cpp
@@ -0,0 +1,49 @@
+/**
+ * Putting all elements from vectors to set
+ */
+#include <iostream>
+
+#include <vector>
+#include <set>
+
+
+ void printDataVector(std::vector <int>& vReceived) {
+    for (int i=0;i<vReceived.size(); ++i) {
+        std::cout << vReceived[i] << " " ;
+    }
+    std::cout << std::endl;
+}
+
+void printDataSet(std::set  <int>& sObj ) {
+
+    for (const auto& i: sObj) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+inline void clearSTL (std::vector <int>& vReceived) {
+        vReceived.clear();
+}
+
+
+int main()
+{
+    std::vector <int> vObj {};
+    std::set  <int> sObj {};
+    for (int i=0;i<5; ++i) {
+        vObj.push_back(i);
+    }
+    std::cout << std::endl;
+    printDataVector(vObj);
+
+    for (const auto& i :vObj ) {
+        sObj.insert(i);
+    }
+    std::cout << std::endl;
+
+    printDataSet(sObj);
+    clearSTL(vObj);
+
+    return (0);
+}
